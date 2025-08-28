95086cabacc7fb38178f885ccddb3c4a547df25e
AmgadSenan <amgadprogram@gmail.com>
2025-08-28 04:22:32 +0300

    Update waseet_cookies.dart

--- lib/waseet_cookies.dart
+++ lib/waseet_cookies.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/services.dart';
 
 class WaseetCookies {
-  static const _ch = MethodChannel('waseetpro/cookies');
+  static const MethodChannel _ch = MethodChannel('waseetpro/cookies');
 
   static Future<String?> getCookies(String url) async {
     return await _ch.invokeMethod<String>('getCookies', {'url': url});
   }
 
   static Future<bool> clearCookies() async {
-    final ok = await _ch.invokeMethod<bool>('clearCookies');
-    return ok ?? false;
+    return (await _ch.invokeMethod<bool>('clearCookies')) ?? false;
   }
 }
